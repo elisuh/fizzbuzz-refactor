775e0be285aef7a80a7fc7158a3e24b800d51c7d
elisuh <elisaverna@gmail.com>
2016-03-26 16:54:27 -0400

    getElementById dom change

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var popup = prompt("Please enter a number from 1 to 100.");
 if (popup != null) {
    for (var i = 1; i <= 100; i++) {
    if (i%15 === 0) {
-     document.innerHTML = "FizzBuzz";
+    document.getElementById("list").innerHTML = "FizzBuzz";
    }
    else if (i%3 === 0) {
-    document.innerHTML = "Fizz";
+    document.getElementById("list").innerHTML = "Fizz";
   }
   else if (i%5 === 0) {
-    document.innerHTML = "Buzz";
+    document.getElementById("list").innerHTML = "Buzz";
   }
   else {
     document.innerHTML = i;
