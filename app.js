03a27f4afd208e3b728609f6a570747259c3942e
elisuh <elisaverna@gmail.com>
2016-03-27 14:25:52 -0400

    added console logs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ function getValue() {
 	var value = prompt("Please enter a number from 1 to 100.");
 	value = parseInt(value);
 	return value;
+	console.log("Is dis working?");
 }
 
 // fizzbuzz loop
@@ -10,15 +11,19 @@ function fizzBuzz() {
    for (var i = 1; i <= 100; i++) {
    if (i%15 === 0) {
     document.getElementById("list").innerHTML = "FizzBuzz";
+    console.log("fizzbuzz");
    }
    else if (i%3 === 0) {
     document.getElementById("list").innerHTML = "Fizz";
+    console.log("fizz");
   }
   else if (i%5 === 0) {
     document.getElementById("list").innerHTML = "Buzz";
+    console.log("buzz");
   }
   else {
     document.getElementById("list").innerHTML = i;
+    console.log(i);
   }
 }
 
@@ -27,7 +32,7 @@ function fizzBuzz() {
       value = prompt("No decimals allowed.");
       value = +value; //return numeric value
       if(isNaN(value)) {
-        prompt("Please enter a whole number!");
+        prompt("Please enter a whole number!"); // none of that NaN funny business.
       }
     }
 
