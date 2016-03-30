73f2b4acef9c57c1c9c2b5acce3356b5f1139130
elisuh <elisaverna@gmail.com>
2016-03-30 19:56:57 -0400

    removed global variable, changed functions

--- app.js
+++ app.js
@@ -1,52 +1,43 @@
-// a variable is born
-var value
-
-// prompt user for value, convert string to number, check for decimals and empty string
-function getValue() {
-	value = prompt("Please enter a number from 1 to 100.");
-	value = parseInt(value);
-    return value;
-   }
-
-// check that it's a whole number
-function validateValue() { 
-  	while(value === "") { // make sure string isn't empty
-      value = prompt("Please type something.");
-  		}
-   while(value%1 != 0) { // check for decimal
-		value =	prompt("No decimals allowed.");
- 		}
-	while(isNaN(value)) {
-		value =	prompt("That is not a number!"); // check for NaN
-      	}
-  return value;
+// prompt user for value, convert string to number
+function getValue(message) {
+  var msg = "Please enter a number from 1 to 100."; // default message
+  if (message) {
+    msg = message; // if the user enters something, prompt with the appropriate message from validateValue function
+  }
+	return parseInt(prompt(msg)); // pars string, convert from prompt to number
 }
+
+function validateValue(num) { //check that number is whole
+  if (num === "") { // check for empty string
+    return getValue("Please type something."); // if empty string, pass this message to getValue
+  } else if (num%1 !== 0) { // check for decimals
+    return getValue("No decimals allowed."); // if decimal, pass this message to getValue
+  } else if (isNaN(num)) { // check for NaN
+    return getValue("That is not a number!"); // if NaN, pass this message to getValue
+  } else {
+    return num; 
+  }
 
 // fizzbuzz loop
 function fizzBuzz(num) {
    for (var i = 1; i <= num; i++) {
    if (i%15 === 0) {
     document.getElementById("list").innerHTML += "FizzBuzz<br>";
-    console.log("fizzbuzz");
    }
    else if (i%3 === 0) {
     document.getElementById("list").innerHTML += "Fizz<br>";
-    console.log("fizz");
   }
   else if (i%5 === 0) {
     document.getElementById("list").innerHTML += "Buzz<br>";
-    console.log("buzz");
   }
   else {
     document.getElementById("list").innerHTML += i + "<br>";
-    console.log(i);
   }
 }
 }
 
 //run functions
-  value = getValue();
-  value = validateValue();
+  var value = validateValue(getValue());
   fizzBuzz(value);
 
 
